01bb1f83ef59cab2edcaa0ba9f811cc07e8627d1
christian_reynaltt <creynaltt@maristak.net>
2024-01-31 00:04:01 +0100

    a dormir

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-    const [number, setNumber] = useState(0)
+    const [subs, setSubs] = useState([
+        {
+            nick: 'dapelu',
+            subMonth: 3,
+            avatar: 'https://i.pravatar.cc/150?u=dapelu',
+            description: 'Dapelu hace de moredador a veces',
+        },
+        {
+            nick: 'Christian',
+            subMonth: 987,
+            avatar: 'https://i.pravatar.cc/150?u=dapelu',
+        },
+    ])
 
-    return <></>
+    return <div className="App"></div>
 }
 
 export default App
