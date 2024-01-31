6417d689397f183e024aab02fdadcbeac8ef6051
christian_reynaltt <creynaltt@maristak.net>
2024-01-31 23:11:48 +0100

    Subs tipado

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,53 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-    const [subs, setSubs] = useState([
-        {
-            nick: 'dapelu',
-            subMonth: 3,
-            avatar: 'https://i.pravatar.cc/150?u=dapelu',
-            description: 'Dapelu hace de moredador a veces',
-        },
-        {
-            nick: 'Christian',
-            subMonth: 987,
-            avatar: 'https://i.pravatar.cc/150?u=dapelu',
-        },
-    ])
+const INITIAL_STATE = [
+    {
+        nick: 'dapelu',
+        subMonth: 3,
+        avatar: 'https://i.pravatar.cc/150?u=dapelu',
+        description: 'Dapelu hace de moredador a veces',
+    },
+    {
+        nick: 'Christian',
+        subMonth: 987,
+        avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',
+    },
+]
 
-    return <div className="App"></div>
+export interface Sub {
+    nick: string
+    subMonth: number
+    avatar: string
+    description: string
+}
+
+function App() {
+    const [subs, setSubs] = useState<Array<Sub>>([])
+
+    React.useEffect(() => {
+        setSubs(INITIAL_STATE)
+    }, [])
+
+    return (
+        <div className="App">
+            <h1>CR subs</h1>
+            <ul>
+                {subs.map((item) => (
+                    <li key={item.nick}>
+                        <img
+                            src={item.avatar}
+                            alt={`Avatar for ${item.nick}`}
+                        />
+                        <h4>
+                            {item.nick} <small>{item.subMonth}</small>
+                        </h4>
+                        <p>{item.description}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App
